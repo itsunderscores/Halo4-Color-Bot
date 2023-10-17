20fe6e6a32a055306c59234044aed06c0ab825f0
Underscores <84211119+itsunderscores@users.noreply.github.com>
2023-10-17 16:53:45 -0400

    Update Halo4ColorBot-TriggerONLY.cpp

--- Halo4ColorBot-TriggerONLY.cpp
+++ Halo4ColorBot-TriggerONLY.cpp
@@ -5,20 +5,53 @@
 #include <random>
 #include <algorithm>
 
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
 bool isr3d(BYTE red, BYTE green, BYTE blue) {
     return red > 200 && green < 100 && blue < 100;
 }
 
+typedef int(*pDD_btn)(int btn);
+pDD_btn      DD_btn;          //Mouse button
+
 int main()
 {
+
+    bool alwayson = false;
+
+
+    string on;
+    cout << "M4 (Y/N): ";
+    cin >> on;
+    if (on == "N") { alwayson = true; }
+
+    cout << "\nBing Bong";
+
     // Set the coordinates of the center box
     int centerX = GetSystemMetrics(SM_CXSCREEN) / 2;
     int centerY = GetSystemMetrics(SM_CYSCREEN) / 2;
     int boxSize = 20; // Adjust the size of the box as needed
     int requiredRedPixels = 5; // Adjust the threshold as needed
 
+    //Load Chinese Mouse Driver
+    HMODULE hDll = LoadLibraryW(L"3jkbnhhjkb.dll");    //App x64
+    if (hDll == nullptr)
+    {
+        return -1;
+    }
+    DD_btn = (pDD_btn)GetProcAddress(hDll, "DD_btn");
+    int st = DD_btn(0);
+    if (st != 1)
+    {
+        //DD Initialize Error
+        return st;
+    }
+
     while (true) {
-        if (GetKeyState(VK_XBUTTON1) & 0x8000) {
+        if (GetKeyState(VK_XBUTTON1) & 0x8000 || alwayson == true) {
             // Capture the screen
             HDC hdcScreen = GetDC(NULL);
             HDC hdcMem = CreateCompatibleDC(hdcScreen);
@@ -49,13 +82,19 @@ int main()
 
             // If the required number of red pixels is detected, simulate a left mouse button click
             if (redPixelCount >= requiredRedPixels) {
-                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+
+                DD_btn(1);
+                cout << ".";
 
                 std::mt19937_64 eng{ std::random_device{}() };
-                std::uniform_int_distribution<> dist{ 50, 100 };
+                std::uniform_int_distribution<> dist{ 30, 90 };
                 std::this_thread::sleep_for(std::chrono::milliseconds{ dist(eng) });
 
-                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                DD_btn(2);
+
+                std::mt19937_64 eng1{ std::random_device{}() };
+                std::uniform_int_distribution<> dist1{ 200, 800 };
+                std::this_thread::sleep_for(std::chrono::milliseconds{ dist1(eng1) });
             }
 
             DeleteObject(hBitmap);
@@ -63,7 +102,7 @@ int main()
             ReleaseDC(NULL, hdcScreen);
 
             std::mt19937_64 eng1{ std::random_device{}() };  // or seed however you want
-            std::uniform_int_distribution<> dist1{ 20, 80 };
+            std::uniform_int_distribution<> dist1{ 20, 30 };
             std::this_thread::sleep_for(std::chrono::milliseconds{ dist1(eng1) });
         }
     }
